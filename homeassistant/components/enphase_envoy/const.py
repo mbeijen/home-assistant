43218a55029c80fce7da140375af0140c25369ff
Arie Catsman <catsmanac@outlook.com>
2023-07-13 22:52:47 +0200

    Enphase-Envoy change 7 day energy entity to total Bring fix for #47 in @briancmpbll to HA Core. 7 day energy is for last 7 days and may go down.

--- homeassistant/components/enphase_envoy/const.py
+++ homeassistant/components/enphase_envoy/const.py
@@ -41,7 +41,7 @@ SENSORS = (
         key="seven_days_production",
         name="Last Seven Days Energy Production",
         native_unit_of_measurement=ENERGY_WATT_HOUR,
-        state_class=SensorStateClass.TOTAL_INCREASING,
+        state_class=SensorStateClass.TOTAL,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
@@ -68,7 +68,7 @@ SENSORS = (
         key="seven_days_consumption",
         name="Last Seven Days Energy Consumption",
         native_unit_of_measurement=ENERGY_WATT_HOUR,
-        state_class=SensorStateClass.TOTAL_INCREASING,
+        state_class=SensorStateClass.TOTAL,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
