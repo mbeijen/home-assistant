aab6b3f352e08e97d1fc879167bd2dad2b089fc6
Arie Catsman <catsmanac@outlook.com>
2023-07-16 13:31:53 +0200

    Replace deprecated energy units

--- homeassistant/components/enphase_envoy/const.py
+++ homeassistant/components/enphase_envoy/const.py
@@ -9,7 +9,12 @@ from homeassistant.components.sensor import (
     SensorEntityDescription,
     SensorStateClass,
 )
-from homeassistant.const import ENERGY_WATT_HOUR, PERCENTAGE, POWER_WATT, Platform
+from homeassistant.const import (
+    UnitOfEnergy, 
+    UnitOfPower, 
+    Platform, 
+    PERCENTAGE
+)
 
 DOMAIN = "enphase_envoy"
 
@@ -27,61 +32,61 @@ SENSORS = (
     SensorEntityDescription(
         key="production",
         name="Current Power Production",
-        native_unit_of_measurement=POWER_WATT,
+        native_unit_of_measurement=UnitOfPower.WATT,
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
         key="daily_production",
         name="Today's Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="seven_days_production",
         name="Last Seven Days Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="lifetime_production",
         name="Lifetime Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="consumption",
         name="Current Power Consumption",
-        native_unit_of_measurement=POWER_WATT,
+        native_unit_of_measurement=UnitOfPower.WATT,
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
         key="daily_consumption",
         name="Today's Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="seven_days_consumption",
         name="Last Seven Days Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="lifetime_consumption",
         name="Lifetime Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="inverters",
         name="Inverter",
-        native_unit_of_measurement=POWER_WATT,
+        native_unit_of_measurement=UnitOfPower.WATT,
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
@@ -100,7 +105,7 @@ SENSORS = (
     SensorEntityDescription(
         key="current_battery_capacity",
         name="Current Battery Capacity",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.ENERGY,
     ),
@@ -109,7 +114,7 @@ SENSORS = (
 BATTERY_ENERGY_DISCHARGED_SENSOR = SensorEntityDescription(
     key="battery_energy_discharged",
     name="Battery Energy Discharged",
-    native_unit_of_measurement=ENERGY_WATT_HOUR,
+    native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
     state_class=SensorStateClass.TOTAL,
     device_class=SensorDeviceClass.ENERGY,
 )
@@ -117,7 +122,7 @@ BATTERY_ENERGY_DISCHARGED_SENSOR = SensorEntityDescription(
 BATTERY_ENERGY_CHARGED_SENSOR = SensorEntityDescription(
     key="battery_energy_charged",
     name="Battery Energy Charged",
-    native_unit_of_measurement=ENERGY_WATT_HOUR,
+    native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
     state_class=SensorStateClass.TOTAL,
     device_class=SensorDeviceClass.ENERGY,
 )
